d0e7702bfe244101ae8bdf6e237dc0266326c853
ZYTsGithub <zhangyt12@yonyou.com>
2019-09-21 16:38:59 +0800

    第一天： // *import和export如何使用，以及import与commondJs中的require有何区别 // *{component},引入对象属性的用法 ：{component} 等价于react.component // *class和extends继承，相当于原本的es5中a.prototype = new b() // *render()函数，在react类中必须实现，只在/state改变后触发！

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//  第一天：
+// *import和export如何使用，以及import与commondJs中的require有何区别
+// *{component},引入对象属性的用法 ：{component} 等价于react.component
+// *class和extends继承，相当于原本的es5中a.prototype = new b()
+// *render()函数，在react类中必须实现，只在/state改变后触发！
 
+// var person = function(){
+//   this.name = "lusa"
+// }
+// var animal = function(height){
+//   this.height = height
+// }
+// person.prototype = new animal(178)
+// console.log(new person())
+
+class animal{
+  constructor(){
+  }
+}
+class person extends animal{
+  constructor(height){
+    super()
+    this.height=height
+
+  }
+ 
+}
+console.log(new person(164))
 function App() {
   return (
     <div className="App">
