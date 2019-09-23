06f0c0f31d6f206bce6d8f1c2d2431f08d78a2cd
ZYTsGithub <zhangyt12@yonyou.com>
2019-09-23 09:19:46 +0800

    第三天：*状态提升，方法下放 *如何更改其他组件的状态：1）将状态属性提升到共同的父组件的状态中 2）在父组件的render中，传递该状态，如：render(){return ( <div><Title themeColor={this.state.themeColor/></div>)} 3)在子组件的render中接收传过来的属性值：this.props.themeColor *方法下放：1）将方法提升到父组件中 2)在父组件的render中传递该方法及其参数，如render(){return ( <div><Title themeColor={this.state.themeColor handleColor={(color)=>this.handleColor(color)}/></div>)}  3)在子组件的render中调用onclick={(color)=>this.props.handleColor("red")}

--- src/App.js
+++ src/App.js
@@ -55,41 +55,41 @@ import Nav from "./zujianImportTry"
 
 // 第二天
 // // *jsx语法细则
-class dianzan extends Component{
-  constructor(){
-    super()
-      this.state={
-        like:false
-      }
-      this.handleClick = this.handleClick.bind(this);
-      // this.handleClick.bind(this)
-      // 记录问题：如果此处不写this.handleClick = this.handleClick.bind(this);而写this.handleClick.bind(this)，
-      // 就会出现 Cannot read property 'setState' of undefined
+// class dianzan extends Component{
+//   constructor(){
+//     super()
+//       this.state={
+//         like:false
+//       }
+//       this.handleClick = this.handleClick.bind(this);
+//       // this.handleClick.bind(this)
+//       // 记录问题：如果此处不写this.handleClick = this.handleClick.bind(this);而写this.handleClick.bind(this)，
+//       // 就会出现 Cannot read property 'setState' of undefined
     
-  }
- handleClick(){
-   console.log(21)
-   console.log(this)
-    this.setState({
-      like:!this.state.like
-    })
- }
+//   }
+//  handleClick(){
+//    console.log(21)
+//    console.log(this)
+//     this.setState({
+//       like:!this.state.like
+//     })
+//  }
  
-//  注意三元运算符在react中的写法
-  render(){
-    return <div><button type="button" style={this.state.like?{color:"red"}:{color:"black"}}  onClick={this.handleClick}>
-      {this.state.like?"喜欢":"已赞"}
-    </button>
-    {/* <Nav title="dosomething">
-      <p>1515</p>
-      <p>895</p>
-    </Nav> */}
-    <Zujian name="greatdeal"/>
-     {/* <Inputs/> */}
-     </div>
-  }
+// //  注意三元运算符在react中的写法
+//   render(){
+//     return <div><button type="button" style={this.state.like?{color:"red"}:{color:"black"}}  onClick={this.handleClick}>
+//       {this.state.like?"喜欢":"已赞"}
+//     </button>
+//     {/* <Nav title="dosomething">
+//       <p>1515</p>
+//       <p>895</p>
+//     </Nav> */}
+//     <Zujian name="greatdeal"/>
+//      {/* <Inputs/> */}
+//      </div>
+//   }
  
-}
+// }
 
 // *组件书写方式有两种
 // 第一种:类的方式,可以继承其他组件的方法
@@ -134,4 +134,50 @@ class dianzan extends Component{
 // 使用时，<p OnClick={this.handleClick}
 // 2)在事件监听处，使用箭头函数 如：<p onClick={()=>this.handleClick()}
 
-export default dianzan;
+
+// 第三天：状态提升，方法下放
+class Button extends Component{
+  render(){
+    const themeColor=this.props.themeColor
+    return(
+      <div> 
+        <button style={{color:themeColor}} onClick={(color)=>this.props.handleColor("red")}>red</button>
+      <button style={{color:themeColor}} onClick={(color)=>this.props.handleColor("green")}>green</button>
+      </div>
+    )
+  }
+}
+
+class Title extends Component{
+  render(){
+    const themeColor=this.props.themeColor
+    return(
+      <p style={{color:themeColor}}>标题</p>
+    )
+  }
+}
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      themeColor:"black"
+    }
+  }
+  handleColor(color){
+    this.setState({
+      themeColor:color
+    })
+  }
+  render(){
+    return(
+     <div> 
+       <h1>App</h1>
+     <Title themeColor={this.state.themeColor} handleColor={(color)=>this.handleColor(color)}></Title>
+     <Button themeColor={this.state.themeColor} handleColor={(color)=>this.handleColor(color)}></Button>
+     </div>
+    )
+  }
+}
+
+
+export default App;
